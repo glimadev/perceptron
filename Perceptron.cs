cc5d42d6d93d42bb58a1bf8c9900c2adbce336ae
glima <glima@vitalitecnologia.com.br>
2016-10-19 23:48:40 -0200

    [DEV] first commit

---
+++ Perceptron.cs
@@ -0,0 +1,103 @@
+﻿
+using System;
+namespace Perceptron
+{
+    public class Perceptron
+    {
+        private const double _threshold = 0.2;
+        private double[,] _inputs;
+        private double[] _weights;
+        private double[] _outputs;
+        private int _epoch = 0;
+
+        public Perceptron(double[,] inputs, double[] outputs)
+        {
+            _inputs = inputs;
+            _outputs = outputs;
+        }
+
+        public int Predict(double[] inputs)
+        {
+            double u = 0;
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                u += inputs[i] * _weights[i];
+            }
+
+            return StepFunction(u);
+        }
+
+        public int Fit()
+        {
+            InitialiseRandomWeights(_inputs);
+
+            bool error = true;
+
+            while (error)
+            {
+                error = false;
+
+                for (int i = 0; i < _inputs.GetLength(0); i++)
+                {
+                    int y = StepFunction(WeightedSum(i));
+
+                    if (y != _outputs[i])
+                    {
+                        Learn(i, y);
+
+                        error = true;
+                    }
+                }
+
+                _epoch++;
+            }
+
+            return _epoch;
+        }
+
+        private void Learn(int i, int y)
+        {
+            for (int w = 0; w < _weights.Length; w++)
+            {
+                double learnCoefficient = _threshold * (_outputs[i] - y);               
+
+                for (int b = 0; b < _inputs.GetLength(1); b++)
+                {
+                    _weights[i] += learnCoefficient * _inputs[i, b];
+                }
+            }
+        }
+
+        private double WeightedSum(int i)
+        {
+            double u = 0;
+
+            for (int j = 0; j < _inputs.GetLength(1); j++)
+            {
+                u += _inputs[i, j] * _weights[i];
+            }
+
+            return u;
+        }
+
+        private int StepFunction(double u)
+        {
+            return (u >= 0.5) ? 1 : 0;
+        }
+
+        private void InitialiseRandomWeights(double[,] inputs)
+        {
+            Random rnd = new Random();
+
+            int i = inputs.GetLength(1);
+
+            _weights = new double[i];
+
+            for (int j = 0; j < i; j++)
+            {
+                _weights[j] = rnd.NextDouble();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] inputs = {
+		        { 1d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		        { 0d, 0d, 0d, 0d, 1d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		        { 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		        { 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		        { 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		        { 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 1d },
+		        { 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 1d, 1d, 0d, 0d, 0d, 1d },
+		        { 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 0d, 1d },
+		        { 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 1d, 1d }
+            };
+
+            double[] outputs = { 1d, 0d, 0d, 1d, 0d, 1d, 0d, 1d, 1d };
+
+            Console.WriteLine(inputs.Length);
+
+            //Fit data 
+            Perceptron perceptron = new Perceptron(inputs, outputs);
+            int epoch = perceptron.Fit();
+            Console.WriteLine(epoch);
+
+            double[] data = { 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 1d, 0d, 1d, 1d };
+            int r = perceptron.Predict(data);
+            Console.WriteLine(r);
+
+            double[] data2 = { 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 1d };
+            int r2 = perceptron.Predict(data2);
+            Console.WriteLine(r2);
+
+            Console.ReadLine();
+        }
+    }
+}
